1194a3c0d601b79dd6917de442042d7c0e50643d
Keilla Vitória <keillavfb@gmail.com>
2024-02-25 21:29:23 -0300

    feat: configurações de swagger 3

--- src/main/kotlin/br/dio/cursos/kotlinspringboot/controller/CreditController.kt
+++ src/main/kotlin/br/dio/cursos/kotlinspringboot/controller/CreditController.kt
@@ -4,6 +4,8 @@ import br.dio.cursos.kotlinspringboot.model.dto.CreditDataDTO
 import br.dio.cursos.kotlinspringboot.model.dto.CreditViewDTO
 import br.dio.cursos.kotlinspringboot.service.impl.CreditConverterService
 import br.dio.cursos.kotlinspringboot.service.impl.CreditService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
+@Tag(name = "Credit", description = "Endpoints for customer credit management")
 @RestController
 @RequestMapping("/api/credits")
 class CreditController (
@@ -23,17 +26,26 @@ class CreditController (
     private val converterService: CreditConverterService
 ){
 
+    @Operation(
+        summary = "Request credit for costumer",
+        description = "Save a credit request for customer")
     @PostMapping
     fun save(@RequestBody @Valid dto: CreditDataDTO): ResponseEntity<CreditViewDTO> {
         var entity = service.save(converterService.dtoToEntity(dto))
         return ResponseEntity(converterService.entityToDto(entity), HttpStatus.CREATED)
     }
 
+    @Operation(
+        summary = "Get credits by customer id",
+        description = "Retrieve all the credits requested by costumer")
     @GetMapping
     fun getAllByCustomerId(@RequestParam(value = "customerId") id: Long): ResponseEntity<List<CreditViewDTO>> {
         return ResponseEntity.ok(converterService.entityToDto(service.findAllByCustomer(id)))
     }
 
+    @Operation(
+        summary = "Get credits by code",
+        description = "Retrieve all the credits requested by code and costumer id")
     @GetMapping("/customer/{id}")
     fun getByCreditCode(@PathVariable id: Long, @RequestParam(value = "creditCode") creditCode: UUID): ResponseEntity<CreditViewDTO> {
         return ResponseEntity.ok(converterService.entityToDto(service.findByCreditCode(id, creditCode)))
--- src/main/kotlin/br/dio/cursos/kotlinspringboot/controller/CustomerController.kt
+++ src/main/kotlin/br/dio/cursos/kotlinspringboot/controller/CustomerController.kt
@@ -5,6 +5,8 @@ import br.dio.cursos.kotlinspringboot.model.dto.CustomerUpdateDTO
 import br.dio.cursos.kotlinspringboot.model.dto.CustomerViewDTO
 import br.dio.cursos.kotlinspringboot.service.impl.CustomerConverterService
 import br.dio.cursos.kotlinspringboot.service.impl.CustomerService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "Customer", description = "Endpoints for customer management")
 @RestController
 @RequestMapping("/api/customers")
 class CustomerController (
@@ -25,21 +28,33 @@ class CustomerController (
     private val converterService: CustomerConverterService
 ) {
 
+    @Operation(
+        summary = "Save costumer",
+        description = "Save a costumer specifying it's fields. A costumer must have a unique email")
     @PostMapping
     fun save(@RequestBody @Valid dto: CustomerDataDTO): ResponseEntity<CustomerViewDTO> {
         val entity = service.save(converterService.dtoToEntity(dto))
         return ResponseEntity(converterService.entityToDto(entity), HttpStatus.CREATED)
     }
 
+    @Operation(
+        summary = "Get customer by id",
+        description = "Retrieve a costumer by its id")
     @GetMapping("/{id}")
     fun get(@PathVariable id: Long) = ResponseEntity.ok(converterService.entityToDto(service.findById(id)))
 
+    @Operation(
+        summary = "Delete customer by id",
+        description = "Deletes a costumer by its id")
     @DeleteMapping("/{id}")
     fun delete(@PathVariable id: Long): ResponseEntity<String> {
         service.delete(id)
         return ResponseEntity("Deleted successfully", HttpStatus.NO_CONTENT)
     }
 
+    @Operation(
+        summary = "Update costumer",
+        description = "Update costumer information. You can only update the following fields: first name, last name, income, zip code and street")
     @PatchMapping
     fun update(@RequestParam(value = "customerId") id: Long,
                @RequestBody @Valid dto: CustomerUpdateDTO):  ResponseEntity<CustomerViewDTO> {
